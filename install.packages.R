3d16142276501a4d0915b0de8fbc2b1ce698c911
Kathleen P Chase <powerskem@gmail.com>
2017-06-22 12:26:00 -0400

    initial commit

---
+++ install.packages.R
@@ -0,0 +1,2 @@
+install.packages(c("MASS", "corrplot", "reshape2", "plyr", "randomForest", "rpart", "rpart.plot", "stringr", "akima", "ggplot2", "dplyr", "gplots", "maptools", "mapdata", "GGally", "igraph"))
+
